6200962d73bb71ae4bf657ba263ec877a98415d5
Bipul Shrestha <bipulshrestha027@gmail.com>
2023-10-08 00:15:10 +0545

    logo and hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 const Header = () => {
     return (
         <header className="w-full">
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+            <nav className="bg-black border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-black">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a href="https://flowbite.com" className="flex items-center">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Trouble Shooters</span>
+                        <img src="../../../../public/images/logo.png" className="h-24 w-[200px]" alt="Flowbite Logo"/>
                     </a>
                     <div className="flex items-center lg:order-2">
                         {/* <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a> */}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,20 @@ const Home = () => {
     return (
         <>
             <Header />
+            <div className="bg-cover bg-center h-screen">
+                <h1 className="text-white absolute ml-40 mt-[200px] text-7xl font-semibold">
+                    Are you lost?
+                 </h1>
+                 <h3 className="absolute ml-40 text-xl text-white mt-80 font-thin text-justify">
+                 Oops, you were thrown out of the solar system, drifting <br></br> amidst the boundless mysteries of deep space, 
+                 <br></br>where every star holds an untold story. <br></br>Will you like to experience the solar system with us?
+                 </h3>
+                <p className="text-white absolute text-opacity-5 text-[400px] ml-[870px]">404</p>
+                 <a href="#" className="text-black bg-white hover:bg-orange-500 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 absolute ml-40 mt-[450px]">Explore...</a>
+                <img className="bg-cover bg-center h-screen w-screen" src="https://w0.peakpx.com/wallpaper/346/240/HD-wallpaper-astronaut-swing-bouquet-space-art-tablet-laptop-background.jpg" alt="Background"></img>
+            </div>
             <div className="grid grid-cols-3 gap-4 lg:p-12 p-6 bg-black">
+                
                 {/* Mercury  Planet*/}
                 <Mercury />
                 {/* Mercury  Planet*/}
