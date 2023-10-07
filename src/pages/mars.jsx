069670779cc9d123d3140937b5e0f71d00e3856a
macbookpro <macbook@macbookpros-MacBook-Pro.local>
2023-10-08 04:09:15 +0545

    Recently updated

--- src/pages/mars.jsx
+++ src/pages/mars.jsx
@@ -1,8 +1,35 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios"; // Import the axios library
+
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const Mars = () => {
-    return (
+
+  // Create state to store the fetched data
+  const [images, setImages] = useState([]);
+
+  // Use useEffect to fetch data from the NASA Images API when the component mounts
+  useEffect(() => {
+    // Define the NASA Images API URL
+    const apiUrl = "https://images-api.nasa.gov/search?q=mars";
+
+    // Make a GET request to the API
+    axios.get(apiUrl)
+      .then((response) => {
+        // Extract the relevant data from the API response
+        const data = response.data.collection.items;
+
+        // Set the fetched data in the state
+        setImages(data);
+      })
+      .catch((error) => {
+        console.error("An error occurred while fetching data:", error);
+      });
+  }, []);
+
+  return (
         <>
         <Header />
         <p className="px-32 pt-9">
@@ -52,6 +79,27 @@ const Mars = () => {
                 <li>Mars' Inexplicable Pop Culture Influence:</li>
                     <p>Mars is so famous that even Earth's candy bar, the Mars Bar, decided to name itself after the planet. Apparently, Mars really knows how to make a celestial impression.</p>
             </ul>
+        </div>
+        <p>Mars</p>
+        
+        {/* Display the fetched images */}
+        <h2 className="text-lg">Image Gallery</h2>
+  
+        <div className="grid grid-cols-3 gap-4 lg:p-12 p-6 bg-black">
+            {images.map((item, index) => (
+                (item.links !== undefined) ? (
+                    <div>
+                    {console.log(item.links[0])}
+                    <img
+                    key={index}
+                    src={item.links[0].href}
+                    alt={`Mars Image ${index}`}
+                    style={{ width: "300px", height: "300px", margin: "10px" }}
+                    />
+                    <div className="text-white flex justify-center">{item.data[0].title}</div>
+                    </div>
+                ) : null
+            ))}
         </div>
         <Footer />
         </>
