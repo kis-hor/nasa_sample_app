468d603d22e62626f92bad07cab8dfbb346add2b
Azam <azamhusain1472@gmail.com>
2023-10-07 14:28:23 +0545

    Earth and Uranus

--- src/components/Uranus.jsx
+++ src/components/Uranus.jsx
@@ -9,14 +9,14 @@ const Uranus = () => {
 
     return(
         <div className="border-4 h-64  bg-black" onClick={toggleShow}>
-        <img className="object-cover h-56 w-auto mx-auto" src="https://media.tenor.com/Cc8vOIn32VgAAAAC/uranus-planet.gif" alt="Uranus" />
+        <img className="object-cover h-56 w-auto mx-auto" src="https://i0.wp.com/i.pinimg.com/originals/50/92/3e/50923e1764ad4fdc3c6ff32e2aaa122d.gif?w=696&ssl=1" alt="Uranus" />
           <span className="flex items-center justify-center text-white">Uranus</span>
           <div id="defaultModal" tabindex="-1" aria-hidden="true" className={`${show ? "" : " hidden"} fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-blur`}>
-            <div className="relative w-full max-w-2xl max-h-full h-[50%] mx-auto my-auto mt-48">
+            <div className="relative w-full max-w-2xl max-h-full h-[50%] mx-auto my-auto">
               <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                   <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                    Earth
+                    Uranus
                   </h3>
                   <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -26,12 +26,24 @@ const Uranus = () => {
                   </button>
                 </div>
                 <div className="p-6 space-y-6">
-                <img className="object-cover h-56 w-auto mx-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/330px-The_Blue_Marble_%28remastered%29.jpg" alt="Earth" />
+                <img className="object-cover h-56 w-auto mx-auto" src="https://i0.wp.com/i.pinimg.com/originals/50/92/3e/50923e1764ad4fdc3c6ff32e2aaa122d.gif?w=696&ssl=1" alt="Earth" />
                   <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    Earth is our mother
+                  Uranus is the seventh planet from the Sun and is a gaseous cyan ice giant. Most of the planet is made of water, ammonia, and methane in a supercritical phase of matter, which in astronomy is called 'ice' or volatiles.Uranus has the third-largest diameter and fourth-largest mass among the Solar System's planets. This planet was discoverd by William Herschel in 13 March 1781.
+
                   </p>
                   <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    It is also our home where we reside
+                  <h1 className="text-3xl mb-3">URANUS</h1>
+                  <ul>
+                  <li><strong>Radius of Uranus:</strong> The radius of Uranus is 25362 km.</li>
+                  <li><strong>Diameter of Uranus:</strong> The diameter of Uranus is 50724 km.</li>
+                  <li><strong>Distance from Sun:</strong> The distance of Uranus from Sun is 2.935 billion km.</li>
+                  <li><strong>Surface Temperature: </strong> The Surface temperature of Uranus is -320F (-195C).</li>
+                  <li><strong>Surface Features:</strong> As an ice giant, Uranus doesn't have a true surface. The planet is mostly swirling fluids. While a spacecraft would have nowhere to land on Uranus, it wouldn't be able to fly through its atmosphere unscathed either.</li>
+                  <li><strong>Rotation:</strong> When Uranus rotates or spins once, it takes around 17 hours to complete one day on the planet. And it takes Uranus approximately 84 Earth years (30,687 Earth days) to complete one orbit of the Sun (or one year in Uranian time).</li>
+                  <li><strong>Orbit:</strong> The orbital period of Uranus is about 84 Earth years.Atmosphere:Uranus' atmosphere is mostly hydrogen and helium, with a small amount of methane and traces of water and ammonia.</li>
+                  <li><strong>Exploration:</strong> Uranus exploration would also allow us to answer more fundamental questions regarding the formation of the Solar System and how it came to be in its current configuration. </li>
+                  </ul>
+
                   </p>
                 </div>
                 <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
