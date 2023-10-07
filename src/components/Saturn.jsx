f1953fb112196693fd6e4e2e899be63036556c0e
macbookpro <macbook@macbookpros-MBP.mshome.net>
2023-10-07 14:05:21 +0545

    jupiter added

--- src/components/Saturn.jsx
+++ src/components/Saturn.jsx
@@ -12,11 +12,11 @@ const Saturn = () => {
         <img className="object-cover h-56 w-auto mx-auto" src="https://i.pinimg.com/originals/eb/24/09/eb2409fa11b564c90d109813b8cd9533.gif" alt="Saturn" />
           <span className="flex items-center justify-center text-white">Saturn</span>
           <div id="defaultModal" tabindex="-1" aria-hidden="true" className={`${show ? "" : " hidden"} fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-blur`}>
-            <div className="relative w-full max-w-2xl max-h-full h-[50%] mx-auto my-auto mt-48">
+            <div className="relative w-full max-w-2xl max-h-full h-[50%] mx-auto my-auto">
               <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                   <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                    Earth
+                    Saturn
                   </h3>
                   <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -26,12 +26,41 @@ const Saturn = () => {
                   </button>
                 </div>
                 <div className="p-6 space-y-6">
-                <img className="object-cover h-56 w-auto mx-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/330px-The_Blue_Marble_%28remastered%29.jpg" alt="Earth" />
+                <img className="object-cover h-56 w-auto mx-auto" src="https://i.pinimg.com/originals/eb/24/09/eb2409fa11b564c90d109813b8cd9533.gif" alt="Saturn" />
                   <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    Earth is our mother
+                  <ul>
+                    <p className="p-4">Saturn is the sixth planet from the sun and the second-largest planet in the solar system. It has a whopping 145 moons. 
+                    It's the farthest planet from Earth that's visible to the unaided eye, but the planet's most outstanding features — its rings — are better viewed through a telescope. 
+                    Although the other gas giants in the solar system — Jupiter, Uranus and Neptune —  also have rings, Saturn's rings are particularly prominent, earning it the nickname the "<b>Ringed Planet.</b>"</p>
+
+                    <ul className="p-4">
+                    <li><b>Radius of Planet:</b> It's radius is 58,232 km. It is 9 times wider than the earth.</li>
+                    <li><b>Distance From Sun:</b> It's distance from the sun is 1.4588 billion km, 9.5 astronomical units away from the Sun.</li>
+                    <li><b>Surface Temperature:</b> The surface temperature of Saturn is -288 degrees Fahrenheit (- 178 degree celsius).</li>
+                    <li><b>Rotation:</b> The Saturn takes 10.7 hours to complete one rotation.</li>
+                    <li><b>Orbit:</b> The Saturn makes complete orbit around the sun in about 29.4 Earth Years.</li>
+                    <li><b>Atmosphere:</b> Saturn's atmosphere is mostly hydrogen and helium.</li>
+                    </ul>
+
+                    
+                    {/* <li><strong>--- Saturn Discovery</strong></li>
+                    <p>Saturn has been known since ancient times and was observed by cultures all over the world. The planet is visible with the naked eye and to ancient cultures, appeared as a bright light shifting among the stars. There are numerous names and mythologies associated with the planet.
+                    The International Astronomical Union, which is the body that represents astronomers around the world, chose the name Saturn to represent the planet. Saturn is associated with the Roman god of agriculture.
+                    In Greek mythology, from which the Romans borrowed, Saturn was called Cronus. Saturn/Cronus is most famous for being one of the Titans, a group of gods who fought with the next generation of gods led by Jupiter (in Roman mythology) or Zeus (in Greek mythology). Your culture may have different stories and monikers associated with Saturn, however.</p>
+                    
+                    <li><strong>--- Saturn's Formation</strong></li>
+                    <p>If you're wondering what type of planet is Saturn, it is a gas giant made up mostly of hydrogen and helium. 
+                    It's volume is greater than <b>760</b> Earths, and it is the second most massive planet in the solar system, about 95 times Earth's mass.
+                    The Ringed Planet is the least dense of all the planets and is the only one less dense than water. 
+                    If there were a bathtub big enough to hold it, Saturn would float.The yellow and gold bands seen in Saturn's atmosphere are the result of superfast winds in the upper atmosphere, which can reach up to <b>1,100 mph (1,800 km/h)</b> around its equator, combined with heat rising from the planet's interior.
+                    Saturn rotates about once every <b>10.5</b> hours. The planet's high-speed spin causes Saturn to bulge at its equator and flatten at its poles. The planet is around <b>75,000 miles (120,000 kilometers)</b> across at its equator, and <b>68,000 miles (109,000 km)</b> from pole to pole. </p> */}
+
+                  </ul>
+
+
                   </p>
                   <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    It is also our home where we reside
+                    
                   </p>
                 </div>
                 <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
