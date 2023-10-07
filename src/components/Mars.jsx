0a5e6eb8cb5e9c54bb2bef36d1f4b68271e2b55f
Aayusha <aayusha098@gmail.com>
2023-10-07 14:18:22 +0545

    mars and neptune

--- src/components/Mars.jsx
+++ src/components/Mars.jsx
@@ -12,11 +12,11 @@ const Mars = () => {
         <img className="object-cover h-56 w-auto mx-auto" src="https://i.pinimg.com/originals/a6/90/27/a69027eb077be2f6a45399adf6f7bbac.gif" alt="Mars" />
           <span className="flex items-center justify-center text-white">Mars</span>
           <div id="defaultModal" tabindex="-1" aria-hidden="true" className={`${show ? "" : " hidden"} fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-blur`}>
-            <div className="relative w-full max-w-2xl max-h-full h-[50%] mx-auto my-auto mt-48">
+            <div className="relative w-full max-w-2xl max-h-full h-[50%] mx-auto my-auto ">
               <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                   <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                    Earth
+                    Mars
                   </h3>
                   <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -26,12 +26,24 @@ const Mars = () => {
                   </button>
                 </div>
                 <div className="p-6 space-y-6">
-                <img className="object-cover h-56 w-auto mx-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/330px-The_Blue_Marble_%28remastered%29.jpg" alt="Earth" />
+                <img className="object-cover h-56 w-auto mx-auto" src="https://i.pinimg.com/originals/a6/90/27/a69027eb077be2f6a45399adf6f7bbac.gif" alt="Earth" />
                   <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    Earth is our mother
+                    Mars is the fourth planet from the sun in our solar system and is often reffered tos as the "Red planet" due to it's reddish appearence, which is caused by iron oxide(rust) on it's surface.
                   </p>
                   <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    It is also our home where we reside
+                    <title>Mars Information</title>
+                    <h1 className="text-3xl text-bold mb-3">Mars</h1>
+                    <ul>
+                      <li><strong>Radius:</strong>Approximately 3,389.5kilometers</li>
+                      <li><strong>Distance from the Sun:</strong>Average distance about 227.9 million kilometer</li>
+                      <li><strong>Diamter:</strong>Approximately 6,779 kilometers</li>
+                      <li><strong>Surface Temperature:</strong>Around -62&deg;celcius</li>
+                      <li><strong>Surface Feature:</strong>Mars is known for its diverse surface features, including vast canyons like Valles Marineris, the largest volcano in the solar system, Olympus Mons, and evidence of ancient river valleys and polar ice caps.</li>
+                      <li><strong>Rotation:</strong>Approximately 24 hours, 39 minutes and 35.244 second</li>
+                      <li><strong>Orbit:</strong>Approximately 687 earth day or about 1.88 earth year</li>
+                      <li><strong>Atmosphere:</strong>Mars has very thin atmosphere with a surface pressure averaging about 0.6% of earth atmospheric pressure at sea level.</li>
+                      <li><strong>Exploration:</strong>Mars has been extensively explored by spacecraft, with notable missions like NASA’s rovers such as Curiosity and Perseverance, aiming to study its geology, search for signs of past life, and prepare for future human missions.</li>
+                    </ul>
                   </p>
                 </div>
                 <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
--- src/components/Neptune.jsx
+++ src/components/Neptune.jsx
@@ -12,11 +12,11 @@ const Neptune = () => {
         <img className="object-cover h-56 w-auto mx-auto" src="https://i.pinimg.com/originals/3c/96/11/3c961145db8f60b5a286f676e3822bf0.gif" alt="Neptune" />
           <span className="flex items-center justify-center text-white">Neptune</span>
           <div id="defaultModal" tabindex="-1" aria-hidden="true" className={`${show ? "" : " hidden"} fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-blur`}>
-            <div className="relative w-full max-w-2xl max-h-full h-[50%] mx-auto my-auto mt-48">
+            <div className="relative w-full max-w-2xl max-h-full h-[50%] mx-auto my-auto ">
               <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                   <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                    Earth
+                    Neptune
                   </h3>
                   <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -28,10 +28,23 @@ const Neptune = () => {
                 <div className="p-6 space-y-6">
                 <img className="object-cover h-56 w-auto mx-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/330px-The_Blue_Marble_%28remastered%29.jpg" alt="Earth" />
                   <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    Earth is our mother
+                    
+                    Neptune is the eighth and farthest known planet from the Sun in our solar system.
                   </p>
                   <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    It is also our home where we reside
+                    <title>Neptune Information</title>
+                    <h1 className="text-3xl text-bold mb-3">Neptune</h1>
+                    <ul>
+                      <li><strong>Radius:</strong>Approximately 24,622 kilometers</li>
+                      <li><strong>Distance from the Sun:</strong>Average distance of about 4.5 billion kilometers</li>
+                      <li><strong>Diameter:</strong>Approximately 49,244 kilometers</li>
+                      <li><strong>Surface Temperature:</strong>Extremely cold, averaging around -214&deg;C</li>
+                      <li><strong>Surface Feature:</strong>Neptune's surface is primarily composed of gases, with no solid surface visible. It's known for its stunning blue color due to the presence of methane in its atmosphere.</li>
+                      <li><strong>Rotation:</strong>Neptune has a rapid rotation, completing one day in about 16 hours and 6 minutes.</li>
+                      <li><strong>Orbit:</strong>Neptune has a long orbit around the sun, taking approximately 165 earth day to complete one orbit.</li> 
+                      <li><strong>Atmosphere:</strong>It has a thick atmosphere primarily composed of hydrogen, helium, and methane.</li> 
+                      <li><strong>Exploration:</strong>Neptune has been primarily studied from a far by telescopes and spacecraft like voyager 2, providing valuable insights into this distant ice gaint.</li>                  
+                      </ul>
                   </p>
                 </div>
                 <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
